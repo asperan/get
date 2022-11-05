2557eec01ff5a993a79986524ec9327db8257a95
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-06 00:00:19 +0100

    feat: add main module

---
+++ lib/get.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Get
+  class Error < StandardError; end
+  # Your code goes here...
+  def self.main
+    puts 'Hello, world!'
+  end
+
+  def self.is_module?
+    true
+  end
+end
