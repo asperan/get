ea342df499e8f1119683da943bd02bafc70b516e
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-07 22:37:48 +0100

    feat: implement base behavior

--- lib/get.rb
+++ lib/get.rb
@@ -1,13 +1,46 @@
 # frozen_string_literal: true
 
+require 'optimist'
+
+require_relative './get/version'
+
 module Get
   class Error < StandardError; end
-  # Your code goes here...
+
+  @@subcommands = { }
+  @@option_parser = Optimist::options do
+    subcommand_max_length = @@subcommands.keys.map { |k| k.to_s.length }.max
+    usage '-h|-v|(<subcommand> [<subcommand-options])'
+    synopsis <<~SUBCOMMANDS unless @@subcommands.empty?
+      Subcommands:
+      #{@@subcommands.keys.map { |k| "  #{k.to_s.ljust(subcommand_max_length)} => #{@@subcommands[k].description}" }.join("\n")}
+    SUBCOMMANDS
+    version "Get version: #{Get::VERSION}"
+    educate_on_error
+    stop_on @@subcommands.keys.map(&:to_s)
+  end
+
   def self.main
-    puts 'Hello, world!'
+    options = @@option_parser.parse
+    error 'No command or option specified' if ARGV.empty?
+    command = ARGV.shift
+    if @@subcommands.include?(command.to_sym)
+      @@subcommands[command.to_sym].action.call
+    else
+      error "Unknown subcommand '#{command}'"
+    end
   end
 
   def self.is_module?
     true
+  end
+
+  def self.error(message)
+    # Change the stdout to stderr for Optimist.educate, as it uses stdout by default
+    # and this method is used in case of errors.
+    $stdout = $stderr
+    puts message.to_s
+    Optimist.educate
+    exit(1)
   end
 end
