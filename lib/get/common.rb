e9e73166e930a363889324cbe980ab824998f7b1
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-08 18:46:02 +0100

    fix: remove output print of in_get_repo?

--- lib/get/common.rb
+++ lib/get/common.rb
@@ -5,7 +5,7 @@ module Common
   # Check if the command is called while in a git repository.
   # If the command fails, it is assumed to not be in a git repository.
   def self.in_git_repo?
-    system('git rev-parse --is-inside-work-tree 2>/dev/null')
+    system('git rev-parse --is-inside-work-tree &>/dev/null')
   end
 
   # Print an error message and optionally run a block.
