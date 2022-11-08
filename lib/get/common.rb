9739101cf3c6e0c6278d28b319d2a158dec44826
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-08 18:25:59 +0100

    feat: add Common module

---
+++ lib/get/common.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Utility module
+module Common
+  # Check if the command is called while in a git repository.
+  # If the command fails, it is assumed to not be in a git repository.
+  def self.in_git_repo?
+    system('git rev-parse --is-inside-work-tree 2>/dev/null')
+  end
+
+  # Print an error message and optionally run a block.
+  # Stdout becomes stderr, so every print is performed to stderr.
+  # This behavior is wanted as this method is called on errors.
+  def self.error(message)
+    $stdout = $stderr
+    puts "Error: #{message}"
+    yield if block_given?
+    exit(1)
+  end
+end
