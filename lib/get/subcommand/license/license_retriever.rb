562330741377f496ae5073fc529df460ce103e31
Alex Speranza <alex.speranza@studio.unibo.it>
2023-03-03 16:21:37 +0100

    feat: add module to retrieve offline or online license texts

---
+++ lib/get/subcommand/license/license_retriever.rb
@@ -0,0 +1,86 @@
+# Get is a toolbox based on git which simplifies the adoption of conventions and some git commands.
+# Copyright (C) 2023  Alex Speranza
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program and the additional permissions granted by
+# the Lesser GPL.  If not, see <https://www.gnu.org/licenses/>.
+
+require 'highline'
+
+# The retrieving module for licenses. It can gather licenses online (from https://choosealicense.com/appendix/)
+# or offline (from a predefined subset of licenses).
+module Retriever
+
+  BASE_OFFLINE_LICENSE_PATH = File.dirname(File.expand_path(__FILE__)) + '/offline_licenses'
+  BASE_ONLINE_LICENSE_URI = 'https://choosealicense.com'
+
+  @@cli = HighLine.new
+
+  def ask_for_license(offline)
+    @@offline = offline
+    list = if @@offline
+             offline_license_list
+           else
+             online_license_list
+           end
+
+    @@cli.puts 'Choose which license you want to use:'
+    choice = @@cli.choose do |menu|
+      menu.flow = :column_down
+      menu.prompt = ''
+      list.each { |element| menu.choice(element) }
+    end
+
+    if @@offline
+      offline_license_text(choice)
+    else
+      online_license_text(choice)
+    end
+  end
+
+  private
+
+  def offline_license_list
+    Dir.children(BASE_OFFLINE_LICENSE_PATH)
+  end
+
+  def online_license_list
+    @@online_licenses = Hash.new
+    text = `curl -fsL "#{BASE_ONLINE_LICENSE_URI}/appendix/" | grep '<th scope="row">'`.strip
+    if $CHILD_STATUS.exitstatus.positive?
+      puts 'WARNING: Unable to retrieve list of online licenses, falling back to offline ones.'
+      @@offline = true
+      offline_license_list
+    else
+      text.split("\n").each do |element|
+        match_result = element.match(/<a href="(.*)">(.*)<\/a>/)
+        @@online_licenses[match_result[2]] = match_result[1]
+      end
+      @@online_licenses.keys
+    end
+  end
+
+  def offline_license_text(license)
+    File.read(BASE_OFFLINE_LICENSE_PATH + '/' + license + '/LICENSE')
+  end
+
+  def online_license_text(license)
+    page = `curl -fsL "#{BASE_ONLINE_LICENSE_URI}#{@@online_licenses[license]}"`
+    Common.error 'Failed to retrieve the license text.' if $CHILD_STATUS.exitstatus.positive?
+
+    match_result = page.match(/<pre id="license-text">(.*)<\/pre>/m)
+    Common.error 'Invalid license text' if match_result[1].nil?
+
+    match_result[1]
+  end
+end
