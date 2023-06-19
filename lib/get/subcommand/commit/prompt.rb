936416006ea0bcbf9582c650e66a80e5cc2cfe50
Alex Speranza <alex.speranza@studio.unibo.it>
2023-06-19 12:01:50 +0200

    refactor: fix instance variables in PromptHandler

--- lib/get/subcommand/commit/prompt.rb
+++ lib/get/subcommand/commit/prompt.rb
@@ -20,13 +20,13 @@
 require 'highline'
 require 'get/commons/git'
 
-# Module for asking to the user informations about a commit message.
+# Module for asking to the user information about a commit message.
 module PromptHandler
-  @@cli = HighLine.new
 
-  @@custom_values_initialized = nil
-  @@custom_types = []
-  @@custom_scopes = []
+  Common.module_instance_value(self, 'cli', 'HighLine.new')
+  Common.module_instance_attr(self, 'custom_values_initialized', false)
+  Common.module_instance_value(self, 'custom_types', [])
+  Common.module_instance_value(self, 'custom_scopes', [])
 
   STRING_VALUE_VALIDATOR = /\s*\S+\s*/
   BODY_END_DELIMITER = "\n\n\n"
@@ -46,14 +46,15 @@ module PromptHandler
 
   def ask_for_type
     extract_types_and_scopes
-    @@cli.choose do |menu|
+
+    MOD_REF.cli.choose do |menu|
       menu.flow = :columns_down
       menu.prompt = 'Choose the type of your commit: '
-      DEFAULT_TYPES.union(@@custom_types).each do |type|
+      DEFAULT_TYPES.union(MOD_REF.custom_types).each do |type|
         menu.choice(type.to_sym)
       end
       menu.choice('Create a new type (rarely needed)') do |_|
-        @@cli.ask('Write the new type to use', String) do |question|
+        MOD_REF.cli.ask('Write the new type to use', String) do |question|
           question.verify_match = true
           question.validate = STRING_VALUE_VALIDATOR
         end
@@ -63,14 +64,14 @@ module PromptHandler
 
   def ask_for_scope
     extract_types_and_scopes
-    @@cli.choose do |menu|
+    MOD_REF.cli.choose do |menu|
       menu.flow = :columns_down
       menu.prompt = 'Choose the scope of your commit: '
-      @@custom_scopes.each do |scope|
+      MOD_REF.custom_scopes.each do |scope|
         menu.choice(scope.to_sym)
       end
       menu.choice('Create a new scope') do |_|
-        @@cli.ask('Write the new scope to use', String) do |question|
+        MOD_REF.cli.ask('Write the new scope to use', String) do |question|
           question.verify_match = true
           question.validate = STRING_VALUE_VALIDATOR
         end
@@ -80,13 +81,13 @@ module PromptHandler
   end
 
   def ask_for_breaking
-    @@cli.agree('Does the commit contain a breaking change? (yes/no) ') do |question|
+    MOD_REF.cli.agree('Does the commit contain a breaking change? (yes/no) ') do |question|
       question.default = false
     end
   end
 
   def ask_for_summary
-    @@cli.ask('The summary of the commit:') do |question|
+    MOD_REF.cli.ask('The summary of the commit:') do |question|
       question.verify_match = true
       question.validate = STRING_VALUE_VALIDATOR
     end
@@ -94,31 +95,33 @@ module PromptHandler
 
   def ask_for_message
     # This method needs a special implementation as the body message can span multiple lines.
-    @@cli.puts('The body of the commit (ends after 3 new lines):')
-    @@cli.input.gets(BODY_END_DELIMITER)
+    MOD_REF.cli.puts('The body of the commit (ends after 3 new lines):')
+    MOD_REF.cli.input.gets(BODY_END_DELIMITER)
   end
 
   private
 
   FIRST_COMMIT = nil
 
+  Common.add_module_self_reference(self)
+
   # This method tries to optimize input parsing by performing multiple operations in one go.
   # So its complexity is a bit higher as it needs to make multiple checks.
   # rubocop:disable Metrics/CyclomaticComplexity
   def extract_types_and_scopes
-    return unless @@custom_values_initialized.nil?
+    return if MOD_REF.custom_values_initialized
 
     Git.with_commit_list_from(FIRST_COMMIT) do |commit_list|
       commit_list.map do |element|
         match = Git::CONVENTIONAL_COMMIT_REGEX.match(element)
         next if match.nil?
 
-        type_already_added = DEFAULT_TYPES.include?(match[1].to_sym) || @@custom_types.include?(match[1])
-        @@custom_types.append(match[1]) unless type_already_added
-        @@custom_scopes.append(match[3]) unless match[3].nil? || @@custom_scopes.include?(match[3])
+        type_already_added = DEFAULT_TYPES.include?(match[1].to_sym) || MOD_REF.custom_types.include?(match[1])
+        MOD_REF.custom_types.append(match[1]) unless type_already_added
+        MOD_REF.custom_scopes.append(match[3]) unless match[3].nil? || MOD_REF.custom_scopes.include?(match[3])
       end
     end
-    @@custom_values_initialized = true
+    MOD_REF.custom_values_initialized = true
   end
   # rubocop:enable Metrics/CyclomaticComplexity
 end
